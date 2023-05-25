5b871f5ebac7d31040070783fcbd8f582661e503
Leticia Leal <leticialealp@hotmail.com>
2023-05-25 19:30:28 -0300

    adicionado dados de contato

--- src/database/contatos.js
+++ src/database/contatos.js
@@ -1,3 +1,4 @@
+import { v4 as gerarId } from 'uuid';
 /*
     Todo contato deverá ter
 
@@ -9,4 +10,145 @@
 
 */
 
-export const contatos = []
+export const contatos = [
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'Maria',
+        telefone: '(51)99988-7799',
+        email: 'maria@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'Pedro',
+        telefone: '(51)99988-7788',
+        email: 'pedro@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'Joana',
+        telefone: '(51)99988-7777',
+        email: 'joana@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'Paulo',
+        telefone: '(51)99988-7755',
+        email: 'paulo@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'José',
+        telefone: '(51)99988-7744',
+        email: 'jose@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'Miguel',
+        telefone: '(51)99988-7733',
+        email: 'miguel@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'Marcos',
+        telefone: '(51)99988-7722',
+        email: 'marcos@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'Carlos',
+        telefone: '(51)99988-7711',
+        email: 'carlos@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'Sergio',
+        telefone: '(51)99988-7700',
+        email: 'sergio@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    },
+    {
+        id: gerarId(),
+        nome: 'João',
+        telefone: '(51)99988-7766',
+        email: 'joao@teste.com',
+        criadoPor: 'leticia@teste.com'
+    }
+]
